25b411a52747c4a3d226c7f9e704697f2266d1f9
Matt Oestreich <matthewpoestreich@gmail.com>
2025-06-20 17:33:15 -0500

    update

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-module.exports = require('node-gyp-build')(__dirname);
+const loader = require('node-gyp-build');
+const native = loader(__dirname);
+
+console.log('✅ Loaded native addon from:', loader.path());
+module.exports = native;

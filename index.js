483eaf4095ea66f3f87fdc8737516475d39283c9
Matt Oestreich <matthewpoestreich@gmail.com>
2025-06-20 19:50:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const path = require("path");
-const packagePath = path.resolve(__dirname)
-module.exports = require('node-gyp-build')(packagePath);
+const packagePath = path.resolve(__dirname);
+module.exports = require("node-gyp-build")(packagePath);
